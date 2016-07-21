8786173f6dd0efb1203aa02b75af0e6f0201cd8b
Sam Lee <sam.lee.dtb@gmail.com>
2016-07-21 13:14:17 -0700

    all buttons working

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-var defaultGrid= 4;
+var defaultGrid= 50;
 var inputGrid = defaultGrid;
 
 $(document).ready(function(){
@@ -7,18 +7,52 @@ $(document).ready(function(){
 	createGrid(defaultGrid);
 
 	/*--------------------------------Click handlers------------------------------*/
+	//reset grid to default
+	$('#reset').click(function(){
+		$(".node").remove();
+		createGrid(defaultGrid);
+	});
+
 	//clear grid
 	$('#clear').click(function(){
 		refreshGrid();
 	});
 
-	//hover-pen mode
+	//hover-pen mode: upon mouse enter, changes the node to black
 	$("#simpleDraw").click(function(){
 		$('.node').mouseenter(function(){
 			$(this).css("background-color", "black");
 		});
 	});
 
+	//rainbow mode: upon mouse enter, changes node to random color
+	$("#rainbow").click(function(){
+		$('.node').mouseenter(function(){
+			$(this).css("background-color", randomColor()); //function returns with brackets!
+		});
+	});
+
+	//trail mode: upon mouse enter, briefly illuminates node
+	$("#trail").click(function(){
+		$(".node").hover(function() {
+			$(this).css("opacity", 1);
+		}, function () {
+			$(this).fadeTo('slow', 0);
+		});
+	});
+
+	//change dimensions
+	$("#dimension").click(function(){
+		$(".node").remove();
+
+		inputGrid = parseInt(prompt("Enter grid length(1-80): ",50),10);
+
+		if (inputGrid > 0&& inputGrid <=80){
+			createGrid(inputGrid);
+		} else{
+			alert("invalid input (grid length must be between 1-80)");
+		}
+	})
 
 });
 
@@ -43,8 +77,7 @@ function randomColor() {
 	var red = Math.floor((Math.random()*255)+1);
 	var green = Math.floor((Math.random()*255)+1);
 	var blue = Math.floor((Math.random()*255)+1);
-	return "rgb(" + red + "," + green + "," + blue + ")";
-
+	return "rgb(" + red + "," + green + "," + blue + ")"; //tricky brackets are tricky!
 }
 
 //clears grid
